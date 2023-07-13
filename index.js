6109f47e553fc01a8f2c1c421bbb4d781bb094ab
winniemei <wmei91@gmail.com>
2023-07-13 14:37:08 -0400

    submit index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 // print "The empty string is falsy (the only falsy string)"
 // else print true
 
-let input = "I am a string";
+// let input = "I am a string";
 
 if (input === false) {
     console.log("The boolean value false is falsy");
@@ -26,4 +26,58 @@ if (input === false) {
     console.log("The empty string is falsy (the only falsy string)");
 } else {
     console.log(true);
+}
+
+// Number Line
+// Declare a variable for num1 and num2
+// If the sum of two numbers is less than -1000
+// Print sum + “is less than -1000”
+// If the sum of two numbers is a negative number
+// Print sum + “is a negative number”
+// If the sum of two numbers is equal to 0
+// Print sum + “is equal to 0”
+// If the sum of two numbers is greater than 100
+// Print sum + “is greater than 100"
+// Else, print sum + “is larger than 0"
+
+let num1 = -5;
+let num2 = 0;
+let sum = num1 + num2;
+if (sum < -1000) {
+    console.log(sum + " is less than -1000");
+} else if (sum < 0) {
+    console.log(sum + " is a negative number");
+} else if (sum === 0) {
+    console.log(sum + " is equal to 0");
+} else if (sum > 100) {
+    console.log(sum + " is greater than 100");
+} else {
+    console.log(sum + " is larger than 0");
+}
+
+// Greater than 5
+// declare two variables called number1 and number2
+// if num1 is greater than or equal to 5 and num 2 is greater than or equal to 5, print true
+// else, print false
+
+let number1 = 5;
+let number2 = 5;
+if (number1 >= 5 && number2 >= 5) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+// Pair and Compare
+// Declare variables for param1A, param1B, param2A, param2B
+// If param1A is strictly equal to param1B or param2A is strictly equal to param2B, print true
+// else, print false
+let param1A = "cake";
+let param1B = "cake";
+let param2A = "pie";
+let param2B = "pie";
+if (param1A === param1B || param2A === param2B) {
+    console.log(true);
+} else {
+    console.log(false);
 }
